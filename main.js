3974b931094ea7bfedb365e52ec99a3af179a0cf
julvaa <163396095+julvaa@users.noreply.github.com>
2024-03-27 14:39:59 +0100

    2ns commit

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-//przy wcisnieciu przycisku submit beda pobierane dane z pola username i wypisane jako naglowek
-// const username = document.getElementById('username');
-// const button = document.getElementById('button'); 
-// const output = document.getElementById('output');
-// const header = document.getElementById('header');
+
 
 const decrease = document.getElementById("decrease");
 const reset = document.getElementById("reset");
@@ -28,17 +24,3 @@ function r(){
  }
  decrease.onclick = d;
 
-
-
-// increase.onclick = function(){
-//     result++;
-//     show.textContent = result;
-// }
-
-// function out(){
-
-//     header.innerHTML = username.value;    
-// }
-// button.addEventListener('click',out);
-
-// document.getElementById("output").innerHTML = 1;
